f9da437bec45877efe0a55d236e95b0dc4dfa943
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-03-15 11:56:40 -0700

    Add MiniProject 1

---
+++ AlgorithmsDataStructures/MiniProject1/main.swift
@@ -0,0 +1,9 @@
+//
+//  Main.swift
+//  AlgorithmsDataStructures
+//
+//  Created by Kazunobu Someya on 2021-03-15.
+//
+
+import Foundation
+
---
+++ AlgorithmsDataStructures/MiniProject2/Main.swift
@@ -0,0 +1,8 @@
+//
+//  Main.swift
+//  AlgorithmsDataStructures
+//
+//  Created by Kazunobu Someya on 2021-03-14.
+//
+
+import Foundation
