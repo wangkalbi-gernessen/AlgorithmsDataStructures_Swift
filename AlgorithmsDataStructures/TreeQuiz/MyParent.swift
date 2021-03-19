167ad91f3af36e0ee95cc48447d26a73a9c2317b
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-03-19 16:53:04 -0700

    Delete pre-order and so on

--- AlgorithmsDataStructures/TreeQuiz/MyParent.swift
+++ AlgorithmsDataStructures/TreeQuiz/MyParent.swift
@@ -30,32 +30,4 @@ func treeTraversal() {
     for i in 0..<n {
         let node = TreeNode(i)
     }
-    
-    func preOrder(_ node: Int) {
-        if node == -1 { return }
-        print(Character(UnicodeScalar(node + 65)!), terminator: "")
-        preOrder(tree[node][0])
-        preOrder(tree[node][1])
-    }
-    
-    func inOrder(_ node: Int) {
-        if node == -1 { return }
-        inOrder(tree[node][0])
-        print(Character(UnicodeScalar(node + 65)!), terminator: "")
-        inOrder(tree[node][1])
-    }
-    
-    func postOrder(_ node: Int) {
-        if node == -1 { return }
-        postOrder(tree[node][0])
-        postOrder(tree[node][1])
-        print(Character(UnicodeScalar(node + 65)!), terminator: "")
-    }
-    
-    preOrder(0)
-    print()
-    inOrder(0)
-    print()
-    postOrder(0)
-    print()
 }
